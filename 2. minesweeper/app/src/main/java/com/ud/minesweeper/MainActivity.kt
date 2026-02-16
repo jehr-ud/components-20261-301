a452f8397dfdab2c159978f62a2c357128da6c52
Jorge HErnandez <jehernandezr@udistrital.edu.co>
2026-02-16 17:55:03 -0500

    First screen advance

--- 2. minesweeper/app/src/main/java/com/ud/minesweeper/MainActivity.kt
+++ 2. minesweeper/app/src/main/java/com/ud/minesweeper/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,8 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MinesweeperTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    LevelScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +34,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LevelScreen(modifier: Modifier = Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MinesweeperTheme {
-        Greeting("Android")
+    Column{
+        Text(
+            text = "Levels:",
+            modifier = modifier
+        )
+
+        Button(
+            onClick = {},
+            modifier = modifier,
+            content = {Text("Low")},
+        )
+
+        Button(
+            onClick = {},
+            modifier = modifier,
+            content = {Text("Medium")},
+        )
+
+
+        Button(
+            onClick = {},
+            modifier = modifier,
+            content = {Text("High")},
+        )
     }
+
 }
