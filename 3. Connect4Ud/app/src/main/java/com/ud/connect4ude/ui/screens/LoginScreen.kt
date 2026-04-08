613a31eb094308a9f6d90aaa79d97675d4ed36a3
estudiante <es.cocoto11@gmial.com>
2026-04-08 13:31:18 -0500

    Cambio visual Inicio de sesión

--- 3. Connect4Ud/app/src/main/java/com/ud/connect4ude/ui/screens/LoginScreen.kt
+++ 3. Connect4Ud/app/src/main/java/com/ud/connect4ude/ui/screens/LoginScreen.kt
@@ -1,8 +1,16 @@
 package com.ud.connect4ude.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -10,43 +18,69 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun LoginScreen(onLoginSuccess: (String, String) -> Unit){
+fun LoginScreen(onLoginSuccess: (String, String) -> Unit) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
-    Column() {
-        Text("Email")
+    val isFormValid = email.isNotBlank() && password.isNotBlank()
 
-        TextField(
+    Column(
+        modifier = Modifier.fillMaxSize().padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Iniciar sesión",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        OutlinedTextField(
             value = email,
             onValueChange = { email = it },
-            label = { Text("Etiqueta") }
+            label = { Text("Correo electronico") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
         )
 
-        Text("password")
 
-        TextField(
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
             value = password,
             onValueChange = { password = it },
-            label = { Text("Etiqueta") }
+            label = { Text("Contraseña") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth()
         )
 
+        Spacer(modifier = Modifier.height(24.dp))
+
         Button(
-            onClick = { onLoginSuccess(email, password) },
+            onClick = { onLoginSuccess(email, password)},
+            enabled = isFormValid,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text("Iniciar Sesión")
+            Text("Ingresar")
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun LoginScreenPrev(){
+fun LoginScreenPrev() {
     LoginScreen(onLoginSuccess =  { email, password -> {}})
 }
