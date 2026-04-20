984e0060adf19d8339937e2a4bedda4caa03859f
Jorge Hernandez <jehernandezrodriguez@gmail.com>
2026-04-20 11:54:25 +0100

    solve error

--- 3. Connect4Ud/app/src/main/java/com/ud/connect4ude/models/Game.kt
+++ 3. Connect4Ud/app/src/main/java/com/ud/connect4ude/models/Game.kt
@@ -2,10 +2,10 @@ package com.ud.connect4ude.models
 
 
 data class Game(
-     val code: String,
-     val status: GameStatus,
-     val player1: String,
-     val player2: String="",
-     val turnPlayerId: String,
-     val winnerPlayerId: String="",
+     val code: String = "",
+     val status: GameStatus = GameStatus.WAITING,
+     val player1: String = "",
+     val player2: String = "",
+     val turnPlayerId: String = "",
+     val winnerPlayerId: String = ""
 )
