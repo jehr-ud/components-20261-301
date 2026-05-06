e4830d8b653aaf338f787b73bbe2944265ab7b8a
Jorge Hernandez <jehernandezr@udistrital.edu.co>
2026-05-06 13:09:15 -0500

    changes to connect with the service

--- 3. Connect4Ud/fronted/app/src/main/java/com/ud/connect4ude/repositories/ConfRepository.kt
+++ 3. Connect4Ud/fronted/app/src/main/java/com/ud/connect4ude/repositories/ConfRepository.kt
@@ -1,5 +1,6 @@
 package com.ud.connect4ude.repositories
 
+import android.util.Log
 import com.ud.connect4ude.network.ApiService
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,13 +13,15 @@ import com.ud.connect4ude.models.GameConfig
 
 class ConfRepository {
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://localhost:3001/")
+        .baseUrl("http://10.0.2.2:3001/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     private val api = retrofit.create(ApiService::class.java)
 
     fun getConfig(game: String, onResult: (GameConfig?) -> Unit) {
+
+        Log.d("serviceapp", "fectch config $game")
 
         val call = api.getConfig(game)
 
@@ -27,6 +30,9 @@ class ConfRepository {
                 call: Call<ConfigResponse>,
                 response: Response<ConfigResponse>
             ) {
+                Log.d("serviceapp", response.body()?.data.toString())
+                Log.d("serviceapp", response.raw().toString())
+
                 if (response.isSuccessful) {
                     onResult(response.body()?.data)
                 } else {
@@ -35,6 +41,8 @@ class ConfRepository {
             }
 
             override fun onFailure(call: Call<ConfigResponse>, t: Throwable) {
+                Log.d("serviceapp", t.message.toString())
+
                 onResult(null)
             }
         })
